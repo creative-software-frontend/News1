cd71efc4d266e7f56f317c82c3d1ccca5c3a750b
Dilouar Hossain <dilouarbd@gmail.com>
2026-02-04 22:57:53 +0600

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#b30000', // প্রথম আলোর মতো লাল রঙ
-      }
-    },
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#b30000', // প্রথম আলোর মতো লাল রঙ
+      }
+    },
+  },
+  plugins: [],
+};
+
